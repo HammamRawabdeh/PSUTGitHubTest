6b083a4ccf59599d72709c549b3c2d59ea7006af
HammamRawabdeh <hammamraw777@gmail.com>
2026-01-12 22:56:22 +0300

    added my name.

--- Test.java
+++ Test.java
@@ -10,6 +10,6 @@ public class Test
         System.out.println ("Firas");
         System.out.println ("Yaza");
         System.out.println ("Lobo");
-        System.out.println ("Hammam");
+        System.out.println ("Hammam.");
     }
 }
